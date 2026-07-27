6d309844720ce12fffaae470a3d928f400c7cec3
alwin hilario <papaalwin159@gmail.com>
2026-07-27 16:10:00 +0800

    enhancement

--- src/components/me/version.tsx
+++ src/components/me/version.tsx
@@ -1,3 +1,3 @@
 export default function Version() {
-  return <div className='text-gray-100 fixed bottom-2 left-2 z-10'>Version: 0.0.4</div>;
+  return <div className='text-sm text-gray-400 fixed bottom-2 left-2 z-20'>Version: 0.0.4</div>;
 }
